a75eb9ec795aed66c1c98e9946c3ba0e69cc438f
Elise De Smet <elise.desmet99@gmail.com>
2019-03-11 15:34:07 +0100

    added some bulls

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "bull.h"
+#include "node.h"
+
+using namespace std;
+
+void print(Node * node){
+    while(node.get_next() != NULLPTR)
+}
+
+int main() {
+    Bull bull1(2, 50, "bull1");
+    Bull bull2(3, 80, "bull2");
+
+    cout << bull1.to_string() << endl;
+    cout << bull2.to_string() << endl;
+
+    Node start(0);
+    Node second(1);
+    Node last(2);
+
+    second.set_next(&last);
+    start.set_next(&second);
+
+    print(&start);
+
+    return 0;
+}
